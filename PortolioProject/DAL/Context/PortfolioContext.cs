b0fce54d5a49d209306ab6e789e5b3b18cc54504
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-08-07 15:09:51 +0300

    Add project files.

---
+++ PortolioProject/DAL/Context/PortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortolioProject.DAL.Entities;
+
+namespace PortolioProject.DAL.Context
+{
+    public class PortfolioContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-GLCB4AC;initial catalog=PortfolioProject;integrated security=true;Encrypt=False;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Expereince> Expereinces { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
